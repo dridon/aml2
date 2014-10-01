db714785f4b6824e715186439c4f2bdf2ab77ac1
BenedicteLC <benedicte.leonard-cannon@mail.mcgill.ca>
2014-10-01 19:05:25 -0400

    Finished RF + other stuff
    Added class to run the classifiers and fixed Bayes bug.

--- Code/classifiers.py
+++ Code/classifiers.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+from math import log
 
 #My implementation is not efficient! Might need to optimize when we use the real dataset.
 class NaiveBayes:
     
-    def train(self, dataset, alpha = 1):
+    def __init__(self, alpha = 1):
+        """       
+        Alpha is used for Laplace smoothing. We could cross-validate over this guy.
+        Typical values ]0,1]
+        """  
+        self.alpha = alpha
+    
+    def train(self, dataset):
         """
         Train the Naive Bayes classifier.
         Takes a dataset of ints with one sample per row,
@@ -20,7 +28,7 @@ class NaiveBayes:
         # Compute p(y=1) for all ys.
         ########
         label_counts = np.bincount(dataset[:,-1]) # Get the number of occurrences of each class, sorted.  
-        self.p_ys = label_counts * 1.0 / len(dataset) # Compute probs. 
+        self.p_ys = np.log(label_counts * 1.0 / len(dataset)) # Compute probs. 
        
         ########
         # Compute p(x|y) for all x,y.
@@ -33,84 +41,105 @@ class NaiveBayes:
         for i in range(self.class_count):  # Loop over each class          
             end_index = start_index + label_counts[i] # end of this class index   
             class_word_counts = np.sum(dataset[start_index:end_index,:-1]) # sum all words of class i    
-            denominator = class_word_counts + alpha * self.feature_count # Here we add the feature_count as Laplace smoothing
+            denominator = class_word_counts + self.alpha * self.feature_count # Here we add the feature_count as Laplace smoothing
             
             for j in range(self.feature_count):  # Loop over each feature
                 single_word_count = np.sum(dataset[start_index:end_index,j]) # sum number times word j appears in class i   
-                numerator = single_word_count + alpha
-                self.p_xi_given_ys[i][j] = numerator * 1.0 / denominator    # Compute p(xi|y)
+                numerator = single_word_count + self.alpha
+                self.p_xi_given_ys[i][j] = log(numerator * 1.0 / denominator)    # Compute p(xi|y)
             
             start_index = end_index
-            
     def test(self, samples):
         """
         Compute P(y|x) for each class, 
         and select the class with highest probability. 
-        Return the array of prediction errors (0:good/1:error)
-        and the prediction accuracy."""
+        Return the array of prediction errors (0:good/1:error),
+        the predicted classes and the prediction accuracy."""
         prediction_errors = np.zeros(len(samples), int)
-        class_predictions = np.zeros(self.class_count)
-                
-        for i in range(len(samples)):    # Loop over each sample
-            for j in range(self.class_count):   # Loop over each class
-                class_predictions[j] = self.p_ys[j]   # Get p(y) for class j
-                for k in range(self.feature_count): # Loop over each feature
-                
-                    # Multiply p(y) by p(xi|y) if the sample entry is non-zero 
-                    if(samples[i][k] != 0):
-                        class_predictions[j] *= self.p_xi_given_ys[j][k]
-                    # Else multiply by 1 - p(xi|y)
-                    else: 
-                        class_predictions[j] *= 1 - self.p_xi_given_ys[j][k]
-                
-            predicted_class = np.argmax(class_predictions)  # Prediction is class with highest probability.
-            
-            # Check if the predicted class doesn't match the true class.
-            if(predicted_class != samples[i][-1]):
-                prediction_errors[i] = 1     
-                
-        # Compute accuracy
-        accuracy = 1 - (np.sum(prediction_errors) * 1.0 / len(prediction_errors))      
-            
-        return prediction_errors, accuracy    
-    
-    def predict(self, samples):
-        """
-        Compute P(y|x) for each class, 
-        and select the class with highest probability. 
-        Return the array of class predictions."""
         predictions = np.zeros(len(samples), int)
         class_predictions = np.zeros(self.class_count)
         
         for i in range(len(samples)):    # Loop over each sample
             for j in range(self.class_count):   # Loop over each class
                 class_predictions[j] = self.p_ys[j]   # Get p(y) for class j
-                for k in range(self.feature_count): # Loop over each feature
                 
-                    # Multiply p(y) by p(xi|y) if the sample entry is non-zero 
-                    if(samples[i][k] != 0):
-                        class_predictions[j] *= self.p_xi_given_ys[j][k]
-                    # Else multiply by 1 - p(xi|y)
-                    else: 
-                        class_predictions[j] *= 1 - self.p_xi_given_ys[j][k]
+                # Multiply p(y) by p(xi|y)                  
+                class_predictions[j] += np.dot(samples[i,:-1], self.p_xi_given_ys[j]) 
+                    
+            predictions[i] = np.argmax(class_predictions)  # Prediction is class with highest probability.
+            
+            # Check if the predicted class doesn't match the true class.
+            if(predictions[i] != samples[i][-1]):
+                prediction_errors[i] = 1     
+                
+        # Compute accuracy
+        accuracy = 1.0 - (np.sum(prediction_errors) * 1.0 / len(prediction_errors))      
+            
+        return prediction_errors, predictions, accuracy    
+    
+    def predict(self, samples):
+        """
+        Compute P(y|x) for each class, 
+        and select the class with highest probability. 
+        Return the array of class predictions.
+        """
+        predictions = np.zeros(len(samples), int)
+        class_predictions = np.zeros(self.class_count)
+        
+        for i in range(len(samples)):    # Loop over each sample
+            for j in range(self.class_count):   # Loop over each class
+                class_predictions[j] = self.p_ys[j]  # Get p(y) for class j               
+                
+                # Multiply p(y) by p(xi|y)                    
+                class_predictions[j] += samples[i,:-1] * self.p_xi_given_ys[j]
                 
             predictions[i] = np.argmax(class_predictions)  # Prediction is class with highest probability.
             
         return predictions    
-   
-# Not finished yet. Working on it.         
+         
 class RandomForest():
-    def __init__(self):        
-        self.random_forest = RandomForestClassifier()#set params here
+    
+    def __init__(self, n_trees=10, criterion='gini', max_depth=None, min_samples_split=2, min_samples_leaf=1, 
+                 max_features='auto', max_leaf_nodes=None, bootstrap=True, oob_score=False, n_jobs=2, random_state=None,
+                 verbose=0, min_density=None, compute_importances=None): 
+        """
+        The random forest has a LOT of parameters. This will be fun to cross-validate.
+        I think scikit learn has a cross-validate function.
+        """       
+        self.random_forest = RandomForestClassifier(n_trees, criterion, max_depth, min_samples_split, min_samples_leaf, 
+                 max_features, max_leaf_nodes, bootstrap, oob_score, n_jobs, random_state,
+                 verbose, min_density, compute_importances)
         
     def train(self, dataset):
+        """
+        Train the Random Forest classifier.
+        Takes a dataset of ints with one sample per row,
+        with the sample's label at the last column.
+        """    
         self.random_forest.fit(dataset[:,:-1], dataset[:,-1])
         
     def test(self, dataset):
-        return self.random_forest.score(dataset[:,:-1], dataset[:,-1])
+        """
+        Test the Random Forest classifier.
+        Takes a dataset of ints with one sample per row,
+        with the sample's label at the last column.
+        Return the array of predicted classes for each sample
+        and the prediction accuracy. 
+        """  
+        predictions = np.zeros(len(dataset), int)
+        
+        accuracy = self.random_forest.score(dataset[:,:-1], dataset[:,-1]) # Predict and compute accuracy.
+        predictions = self.predict(dataset[:,:-1]) # Predict and return list of predictions.
+        
+        return predictions, accuracy
         
     def predict(self, samples):
-        self.random_forest.predict(samples)
+        """
+        Predict using the Random Forest classifier.
+        Takes a test set of ints with one sample per row.
+        Return the array of predicted classes for each sample.
+        """  
+        return self.random_forest.predict(samples)
 
 def toy_data():
     """
@@ -134,15 +163,5 @@ def toy_data():
             #dataset[i][j] = np.random.randint(0,10)  
     dataset = np.column_stack((dataset, Y))
     return (dataset)       
-
-# Testing
-#c = NaiveBayes()
-dataset = toy_data()
-#c.train(dataset,1)   
-#predictions = c.predict(dataset)  
-#print predictions
-f = RandomForest()
-f.train(dataset)
-print f.test(dataset)
 
    
---
+++ Code/run_classifiers.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+
+from numpy import genfromtxt, newaxis
+import numpy as np
+from sklearn.metrics import confusion_matrix
+from classifiers import NaiveBayes, RandomForest, toy_data
+from sklearn.cross_validation import StratifiedKFold
+import matplotlib.pyplot as plt
+
+def load_dataset(file_name, var_type):
+    """
+    Load a csv file and converts it to
+    a 2D numpy array. Assumes each line is a sample
+    and the last column contains the labels.
+    Takes a csv filename and the variable type.
+    """
+    if type(file_name) == str:
+        # open the file
+        f = open(file_name, 'r')
+    else:
+        # then file_name is an iterable object
+        f = file_name        
+    
+    dataset = genfromtxt(f, var_type, delimiter=',')
+    
+    return dataset
+    
+def get_confusion_matrix(Y1, Y2):    
+    """
+    Generate the confusion matrix between
+    the expected outputs and the classifier outputs.    
+    Y1: 1D array of expected outputs.
+    Y2: 1D array of classifier outputs.
+    """    
+    # Compute confusion matrix
+    matrix = confusion_matrix(Y1, Y2)
+    
+    print(matrix)
+    
+    # Show confusion matrix in a separate window.
+    plt.matshow(matrix)
+    plt.title('Confusion matrix')
+    plt.colorbar()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+    
+def get_k_fold_partition(dataset, k = 5):
+    """
+    Divides a dataset into a training set
+    and a validation set. 
+    The folds are made by preserving the percentage of samples for each class.
+    Takes the dataset and k as parameters.
+    Return the train set and the test set.
+    """   
+    if k < 2:  # k must be at least 2
+        k = 2
+        
+    samples = dataset[:,:-1]    
+    labels = dataset[:,-1]  
+    folds = StratifiedKFold(labels, k) # Generate the fold indexes.
+    
+    # Build the 2 sets:
+    for train_index, test_index in folds:
+        X_train, X_test = samples[train_index], samples[test_index]
+        y_train, y_test = labels[train_index], labels[test_index]
+        
+    train_set = np.concatenate((X_train, y_train[:,newaxis]), axis = 1)
+    test_set = np.concatenate((X_test, y_test[:,newaxis]), axis = 1)
+            
+    return train_set, test_set
+    
+if __name__ == "__main__":    
+   
+    dataset = load_dataset("Training.csv", int)       
+    
+    # Testing    
+    training_set, validation_set = get_k_fold_partition(dataset, 5)
+    #print training_set
+    #print validation_set         
+    
+    print "Training the NB classifier..."   
+    c = NaiveBayes() 
+    c.train(training_set)       
+    print "Testing the NB classifier..."  
+    errors, predictions, accuracy = c.test(validation_set)
+    print "NB accuracy: ", accuracy 
+    print "NB confusion matrix:" 
+    get_confusion_matrix(validation_set[:,-1], predictions)
+     
+    print " "
+     
+    print "Training the RF classifier..."     
+    f = RandomForest()
+    f.train(training_set)
+    print "Testing the RF classifier..." 
+    predictions, accuracy = f.test(validation_set)
+    print "RF accuracy: ", accuracy 
+    print "RF confusion matrix:"
+    get_confusion_matrix(validation_set[:,-1], predictions)
+    
+    
