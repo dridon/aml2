728667d01f69f81e40c5f5872d9d0b435b49416d
Faiz Khan <dridon@gmail.com>
2014-10-13 11:13:15 -0400

    merge

--- Code/run_classifiers.py
+++ Code/run_classifiers.py
@@ -72,7 +72,7 @@ def get_k_fold_partition(samples, labels, k = 5):
     
 if __name__ == "__main__":    
 
-    samples, labels = ftg.get_features(stemming=False, test=False, inbool=False, limit = 1000)
+    samples, labels = ftg.get_features(stemming=False, test=False, inbool=False, limit = 2000)
     
     # Testing    
     training_set, validation_set = get_k_fold_partition(samples, labels, 5)
